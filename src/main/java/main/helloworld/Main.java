25ef145d18acbcbc15a39996c39db3027ecbcb81
Nenad <maksicnenad97@gmail.com>
2020-02-06 19:58:43 +0100

    adds examples for loops

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -15,120 +15,167 @@ public class Main {
         /*
         System.out.println("Hello World");
         System.out.println("Nenad Maksic");
-        
         int age = 5;
         if (age > 18){
-            System.out.println("Osoba je punoletna");
+        System.out.println("Osoba je punoletna");
         } else {
-            System.out.println("Osoba je maloletna");
+        System.out.println("Osoba je maloletna");
         } */
-
-              /*
+        /*
         System.out.print("Type your age number");
         Scanner input = new Scanner(System.in);
         int userAge = input.nextInt(); */
-       /*
+        /*
         System.out.println("Korisnik je uneo: " + userAge);
-        
         if (userAge > 18){
-            System.out.println("Osoba je punoletna");
+        System.out.println("Osoba je punoletna");
         } else {
-             System.out.println("Osoba je maloletna");
+        System.out.println("Osoba je maloletna");
         }
-        */
-       /* 
-       
+         */
+        /*
         if (userAge >= 0 && userAge <=5 ) {
-            System.out.println("Baby");
+        System.out.println("Baby");
         } else if (userAge >=6 && userAge <=11) {
-            System.out.println("Kid");
+        System.out.println("Kid");
         } else if (userAge >=12 && userAge <=17){
-            System.out.println("Teen");
+        System.out.println("Teen");
         } else if (userAge >18 && userAge <=150s) {
-            System.out.println("Adult");
+        System.out.println("Adult");
         } else {
-            System.out.println("Invalid age number");
+        System.out.println("Invalid age number");
         } */
-       
-       /*
-         System.out.print("Type your age number");
+        /*
+        System.out.print("Type your age number");
         Scanner input = new Scanner(System.in);
         int userAge = input.nextInt();
         switch ((userAge >=0 && userAge <=5) ? 0 :
-                (userAge >=6 && userAge <=11) ? 1:
-                (userAge >=12 && userAge <=17) ? 2:
-                (userAge >=18 && userAge <=150) ? 3: 4) {
-            case 0:
-                  System.out.println("Baby");
-                  break;
-            case 1:
-                System.out.println("Kid");
-                break;
-            case 2:    
-                System.out.println("Teen");
-                break;
-            case 3:   
-                 System.out.println("Adult");
-                 break;
-            case 4:
-                System.out.println("Invalid age number");
-                break; 
+        (userAge >=6 && userAge <=11) ? 1:
+        (userAge >=12 && userAge <=17) ? 2:
+        (userAge >=18 && userAge <=150) ? 3: 4) {
+        case 0:
+        System.out.println("Baby");
+        break;
+        case 1:
+        System.out.println("Kid");
+        break;
+        case 2:
+        System.out.println("Teen");
+        break;
+        case 3:
+        System.out.println("Adult");
+        break;
+        case 4:
+        System.out.println("Invalid age number");
+        break; 
         }
-       */
-       
-       /*
+         */
+        /*
         System.out.print("Unesite redni broj dana u nedelji 1-7");
         Scanner input = new Scanner(System.in);
         int day = input.nextInt();
-        
         switch (day) {
-            case 1:
-                System.out.print("Ponedeljak");
-                break;
-            case 2:
-                System.out.print("Utorak");
-                break;
-            case 3:
-                System.out.print("Sreda");
-                break;
-            case 4:
-                System.out.print("Cetvrtak");
-                break;
-            case 5:
-                System.out.print("Petak");
-                break;
-            case 6:
-                System.out.print("Subota");
-                break;
-            case 7: 
-                System.out.print("Nedelja");
-                break;
-           
+        case 1:
+        System.out.print("Ponedeljak");
+        break;
+        case 2:
+        System.out.print("Utorak");
+        break;
+        case 3:
+        System.out.print("Sreda");
+        break;
+        case 4:
+        System.out.print("Cetvrtak");
+        break;
+        case 5:
+        System.out.print("Petak");
+        break;
+        case 6:
+        System.out.print("Subota");
+        break;
+        case 7:
+        System.out.print("Nedelja");
+        break;
         } */
-       /*
+        /*
         String[] robneMarke = {"Nike", "Adidas", "Reebok", "UnderArmour" };
         for (String i : robneMarke){
-            System.out.println(i);
+        System.out.println(i);
         }
          */
-       
-       /*
-       Scanner input = new Scanner(System.in);
-       System.out.print("Unesite koliko redova zelite da ima vasa jelka: ");
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.print("Unesite koliko redova zelite da ima vasa jelka: ");
         int cTree = input.nextInt();
         input.close();
-        
-        
         for (int i = 0; i < cTree; i++) {
-           for (int j = 0; j < cTree - i; j++) 
-                System.out.print(" ");
+        for (int j = 0; j < cTree - i; j++)
+        System.out.print(" ");
+        for (int k = 0; k < (2 * i + 1); k++)
+        System.out.print("*");
+        System.out.println();
+        }
+         */
+//       int x = 5;
+//       while(x<10) {
+//           System.out.println(x);
+//            x++;
+//       }
+//
+//       while(x>0){
+//           System.out.println(x);
+//           x--;
+//       }
+//
+//        int x = 15;
+//        do {
+//            System.out.println(x);
+//            x--;
+//        } while (x>12);
+        
+//          for (int i = 20; i >=10; i--) {
+//              System.out.println(i);
+//        }
+//
+//          for (int i = 20; i<30; i++) {
+//              System.out.println(i);
+//        }
+          
+          //kvadrat:
+//         
+//            for (int i = 0; i < 10; i++) {
+//                 for (int j = 0; j < 10; j++) {
+//                  System.out.print("*");
+//              }
+//              System.out.println();
+//            }
+        
+          
+          
+                
+//             for (int i = 0; i < 10; i++) {
+//                  for (int j = 0; j < 10; j++) {
+//                    System.out.print("(" + i + "," + j + ") ");
+//              }
+//              System.out.println();
+//            }
+//          
+//           for (int i = 1; i <= 10; i++) {
+//                for (int j = 1; j <= 10; j++) {
+//                    System.out.print("(" + i + "," + j + ") ");
+//              }
+//              System.out.println();
+//            }
+             
             
-                for (int k = 0; k < (2 * i + 1); k++) 
-                    System.out.print("*");
-                    System.out.println();
-                } 
-                */
+
             
+          
+             
+             
+             
+             
+             
         }
         
         
