0f75813ff24b4295857f20f4c03019442a15f127
maksicQA <maksicnenad97@gmail.com>
2020-02-06 12:38:02 +0100

    Adds exercise with days in week

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -23,10 +23,10 @@ public class Main {
             System.out.println("Osoba je maloletna");
         } */
 
-              
+              /*
         System.out.print("Type your age number");
         Scanner input = new Scanner(System.in);
-        int userAge = input.nextInt();
+        int userAge = input.nextInt(); */
        /*
         System.out.println("Korisnik je uneo: " + userAge);
         
@@ -44,16 +44,20 @@ public class Main {
             System.out.println("Kid");
         } else if (userAge >=12 && userAge <=17){
             System.out.println("Teen");
-        } else if (userAge >18) {
+        } else if (userAge >18 && userAge <=150s) {
             System.out.println("Adult");
         } else {
             System.out.println("Invalid age number");
         } */
-        
+       
+       /*
+         System.out.print("Type your age number");
+        Scanner input = new Scanner(System.in);
+        int userAge = input.nextInt();
         switch ((userAge >=0 && userAge <=5) ? 0 :
                 (userAge >=6 && userAge <=11) ? 1:
                 (userAge >=12 && userAge <=17) ? 2:
-                (userAge >=18) ? 3: 4) {
+                (userAge >=18 && userAge <=150) ? 3: 4) {
             case 0:
                   System.out.println("Baby");
                   break;
@@ -68,11 +72,49 @@ public class Main {
                  break;
             case 4:
                 System.out.println("Invalid age number");
-                break;
-                
+                break; 
         }
+       */
+       
+       /*
+        System.out.print("Unesite redni broj dana u nedelji 1-7");
+        Scanner input = new Scanner(System.in);
+        int day = input.nextInt();
+        
+        switch (day) {
+            case 1:
+                System.out.print("Ponedeljak");
+                break;
+            case 2:
+                System.out.print("Utorak");
+                break;
+            case 3:
+                System.out.print("Sreda");
+                break;
+            case 4:
+                System.out.print("Cetvrtak");
+                break;
+            case 5:
+                System.out.print("Petak");
+                break;
+            case 6:
+                System.out.print("Subota");
+                break;
+            case 7: 
+                System.out.print("Nedelja");
+                break;
+           
+        } */
         
         
-    }
+        
+                
+        
+        
+        
+        
+        
+        
+        }
     
-}
+    }
